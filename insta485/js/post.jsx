1796283a02f8080f217bc99284cb014ab60f20a0
Farhan <farharif@umich.edu>
2023-02-10 21:07:24 -0800

    Fix styles, tinggal pylint and eslint

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import OnePost from './OnePost'
+import OnePost from "./OnePost";
 
 export default function Post({ url }) {
   const [posts, setPosts] = useState([]);
@@ -23,20 +23,14 @@ export default function Post({ url }) {
     };
   }, [url]);
 
-  const rows = []
+  const rows = [];
   posts.map((post) => {
-    rows.push(<OnePost key = {post.postid} post= {post}/>);
-  }
-  );
+    rows.push(<OnePost key={post.postid} post={post} />);
+  });
 
-  return (
-    <>
-      {rows}
-    </>
-  );
+  return <>{rows}</>;
 }
 
 Post.propTypes = {
   url: PropTypes.string.isRequired,
 };
-
